09a4ab026e8f9f8111796e8a578d84c6fc5456b5
TannerDryden <tdd7197@gmail.com>
2022-05-01 16:50:26 -0700

    Added menu

---
+++ TRNBulletHell/Game/cButton.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Content;
+
+namespace TRNBulletHell.Game
+{
+    public class cButton
+    {
+        Texture2D texture;
+        SpriteFont font;
+        String text;
+        int wordOffset;
+        Vector2 position;
+        //Rectangle rectangle;
+
+        Color color = new Color(255, 255, 255, 255);
+
+        public Vector2 size;
+
+        public virtual Rectangle Rectangle
+        {
+            get
+            {
+                return new Rectangle((int)position.X, (int)position.Y, (int)texture.Width/3 , (int)texture.Height/3 );
+            }
+        }
+
+        public cButton(Texture2D newTexture, GraphicsDevice graphics, SpriteFont newFont, String newText, int newWordOffset)
+        {
+            texture = newTexture;
+            font = newFont;
+            text = newText;
+            wordOffset = newWordOffset;
+            //ScreenW = 800, ScreenH = 600
+            //ImgW    = 100, ScreenH = 20
+            //size = new Vector2(graphics.Viewport.Width, graphics.Viewport.Height);
+
+        }
+
+        bool down;
+        public bool isClicked;
+        public void Update(MouseState mouse)
+        {
+            //rectangle = new Rectangle((int)position.X, (int)position.Y, (int)texture.Width, (int)texture.Height);
+            Rectangle mouseRectangle = new Rectangle(mouse.X, mouse.Y, 1, 1);
+            
+            if(mouseRectangle.Intersects(Rectangle))
+            {
+                if (color.A == 255) down = false;
+                if (color.A == 0) down = true;
+                if (down) color.A += 5; else color.A -= 5;
+                if (mouse.LeftButton == ButtonState.Pressed) isClicked = true;
+            }
+            else if (color.A < 255)
+            {
+                color.A += 5;
+                isClicked = false;
+            }
+            
+        }
+
+        private int getXCenter()
+        {
+            return (int)position.X + Rectangle.Width / 2 - wordOffset;
+        }
+
+        private int getYCenter()
+        {
+            return (int)position.Y + Rectangle.Height / 2 - 20;
+        }
+
+        public void setPosition(Vector2 newPosition)
+        {
+            position = newPosition;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            //spriteBatch.Draw(texture, rectangle, color);
+            
+            spriteBatch.Draw(texture, Rectangle, color);
+            spriteBatch.DrawString(font, text, new Vector2(getXCenter(), getYCenter()), Color.White);
+        }
+    }
+}
--- TRNBulletHell/GameDriver.cs
+++ TRNBulletHell/GameDriver.cs
@@ -45,11 +45,21 @@ namespace TRNBulletHell
         CollisionDetection collisionDetection = new CollisionDetection();
         EntityLists entities = EntityLists.Instance;
 
+        enum GameState
+        {
+            MainMenu,
+            Playing,
+            Options,
+            Quit,
+        }
+
+        GameState CurrentGameState = GameState.MainMenu;
+        cButton btnPlay, btnOptions, btnQuit, btnMenu;
+
         public GameDriver()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
-            IsMouseVisible = false;
             _graphics.IsFullScreen = false;
         }
 
@@ -73,6 +83,20 @@ namespace TRNBulletHell
             playerBullet2D = Content.Load<Texture2D>("bullet");
             enemyBullet = Content.Load<Texture2D>("EnemyBullet");
 
+            // Buttons
+            btnPlay = new cButton(Content.Load<Texture2D>("Button"), _graphics.GraphicsDevice, font, "Play", 30);
+            btnPlay.setPosition(new Vector2(325, 100));
+
+            btnOptions = new cButton(Content.Load<Texture2D>("Button"), _graphics.GraphicsDevice, font, "Options", 55);
+            btnOptions.setPosition(new Vector2(325, 200));
+
+            btnQuit = new cButton(Content.Load<Texture2D>("Button"), _graphics.GraphicsDevice, font, "Quit", 30);
+            btnQuit.setPosition(new Vector2(325, 300));
+
+            btnMenu = new cButton(Content.Load<Texture2D>("Button"), _graphics.GraphicsDevice, font, "Back", 35);
+            btnMenu.setPosition(new Vector2(325, 400));
+
+
             // Next Deliverable a class that reads the JSON file will define the waves and the quantity of the waves for a longer Game Play.
             first = new Wave(0, 3, "EnemyA", enemyATexture);
             second = new Wave(30, 1, "MidBoss", midBossTexture);
@@ -86,58 +110,75 @@ namespace TRNBulletHell
 
         protected override void Update(GameTime gameTime)
         {
-            //for clock display
-            minutes = gameTime.TotalGameTime.Minutes;
-            seconds = gameTime.TotalGameTime.Seconds;
+            MouseState mouse = Mouse.GetState();
 
-            //Set delay for spawning.
-            var timer = (float)gameTime.ElapsedGameTime.TotalSeconds;
-
-            _remainingDelay -= timer;
-
-        
-
-
-            double waveTimer = gameTime.TotalGameTime.TotalSeconds;
-           // Wave first = new Wave(0, 3, "EnemyA", enemyATexture);
-            if (first.createWave(waveTimer, _remainingDelay, enemyBullet) || 
-                second.createWave(waveTimer, _remainingDelay, enemyBullet) || 
-                third.createWave(waveTimer, _remainingDelay, enemyBullet) || 
-                fourth.createWave(waveTimer, _remainingDelay, enemyBullet))
+            switch (CurrentGameState)
             {
-                _remainingDelay = _delay;
+                case GameState.MainMenu:
+                    this.IsMouseVisible = true;
+                    if (btnPlay.isClicked) CurrentGameState = GameState.Playing;
+                    if (btnOptions.isClicked) CurrentGameState = GameState.Options;
+                    if (btnQuit.isClicked) CurrentGameState = GameState.Quit;
+                    btnPlay.Update(mouse);
+                    btnOptions.Update(mouse);
+                    btnQuit.Update(mouse);
+                    break;
+
+                case GameState.Options:
+                    this.IsMouseVisible = true;
+                    if (btnMenu.isClicked) CurrentGameState = GameState.MainMenu;
+                    btnMenu.Update(mouse);
+                    break;
+
+                case GameState.Quit:
+                    Exit();
+                    break;
+
+                case GameState.Playing:
+                    this.IsMouseVisible = false;
+                    //for clock display
+                    minutes = gameTime.TotalGameTime.Minutes;
+                    seconds = gameTime.TotalGameTime.Seconds;
+
+                    //Set delay for spawning.
+                    var timer = (float)gameTime.ElapsedGameTime.TotalSeconds;
+                    _remainingDelay -= timer;
+
+                    // Create waves
+                    double waveTimer = gameTime.TotalGameTime.TotalSeconds;
+                    if (first.createWave(waveTimer, _remainingDelay, enemyBullet) ||
+                        second.createWave(waveTimer, _remainingDelay, enemyBullet) ||
+                        third.createWave(waveTimer, _remainingDelay, enemyBullet) ||
+                        fourth.createWave(waveTimer, _remainingDelay, enemyBullet))
+                    {
+                        _remainingDelay = _delay;
+                    }
+
+                    // mark final boss entry
+                    if (gameTime.TotalGameTime.TotalSeconds >= 120 && _remainingDelay <= 0 && finalBossCount < 1)
+                    {
+                        finalBossCount++;
+                    }
+
+                    // check if boss is dead or game is over
+                    if (finalBossCount >= 1 && (gameTime.TotalGameTime.TotalSeconds >= 150 || EntityLists.enemyList.ToArray().Length == 0))
+                    {
+                        win = true;
+                    }
+
+                    // exit if esc pressed
+                    if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+                    {
+                        Exit();
+                    }
+
+                    // update all entities
+                    EntityLists.Update(gameTime);
+
+                    // detect collisions
+                    collisionDetection.detectCollision(gameTime);
+                    break;
             }
-
-
-            if (gameTime.TotalGameTime.TotalSeconds >= 120 && _remainingDelay <= 0 && finalBossCount < 1)
-            {
-                finalBossCount++;
-                
-            }
-            
-            // check if boss is dead or game is over
-            if(finalBossCount >= 1 && (gameTime.TotalGameTime.TotalSeconds >= 150 || finalBossDead()))
-            {
-                win = true;
-            }
-
-            bool finalBossDead()
-            {
-                return (EntityLists.enemyList.ToArray().Length == 0);
-            }
-
-            KeyboardState state = Keyboard.GetState();
-
-            if (state.IsKeyDown(Keys.Escape))
-            {
-                Exit();
-            }
-
-            // update all entities
-            EntityLists.Update(gameTime);
-
-            // detect collisions
-            collisionDetection.detectCollision(gameTime);
 
             base.Update(gameTime);
         }
@@ -148,38 +189,55 @@ namespace TRNBulletHell
             _spriteBatch.Begin();
             _spriteBatch.Draw(backgroundSprite, new Vector2(0, 0), Color.White);
 
-            EntityLists.Draw(_spriteBatch);
+            switch (CurrentGameState)
+            {
+                case GameState.MainMenu:
+                    _spriteBatch.DrawString(font, "Bullet Hell Game", new Vector2(300, this.Window.ClientBounds.Height / 100), Color.White);
+                    btnPlay.Draw(_spriteBatch);
+                    btnOptions.Draw(_spriteBatch);
+                    btnQuit.Draw(_spriteBatch);
+                    break;
 
-            if (seconds < 10)
-            {
-                _spriteBatch.DrawString(font, $"{minutes + " : 0" + seconds}", new Vector2(700, 0), Color.White);
-            }
-            else
-            {
-                _spriteBatch.DrawString(font, $"{minutes + " : " + seconds}", new Vector2(700, 10), Color.White);
-            }
+                case GameState.Options:
+                    _spriteBatch.DrawString(font, "Options", new Vector2(350, this.Window.ClientBounds.Height / 100), Color.White);
+                    btnMenu.Draw(_spriteBatch);
+                    break;
 
-            // display health if player(s) is alive
-            if(EntityLists.playerList.Count != 0)
-            {
-                _spriteBatch.DrawString(font, $"Health: { EntityLists.playerList[0].GetHealth().ToString()}", new Vector2(20, 10), Color.White);
-            }
-            else
-            {
-                _spriteBatch.DrawString(font, "Game Over", new Vector2(325, this.Window.ClientBounds.Height / 2), Color.White);
-                _spriteBatch.DrawString(font, "Press ESC to exit", new Vector2(300, this.Window.ClientBounds.Height / 2 + 100), Color.White);
-                EntityLists.enemyList.Clear();
-                EntityLists.playerBulletList.Clear();
-                EntityLists.enemyBulletList.Clear();
-            }
+                case GameState.Playing:
+                    EntityLists.Draw(_spriteBatch);
 
-            if(win)
-            {
-                _spriteBatch.DrawString(font, "Winner", new Vector2(325, this.Window.ClientBounds.Height / 2), Color.White);
-                _spriteBatch.DrawString(font, "Press ESC to exit", new Vector2(300, this.Window.ClientBounds.Height / 2 + 100), Color.White);
-                EntityLists.enemyList.Clear();
-                EntityLists.playerBulletList.Clear();
-                EntityLists.enemyBulletList.Clear();
+                    if (seconds < 10)
+                    {
+                        _spriteBatch.DrawString(font, $"{minutes + " : 0" + seconds}", new Vector2(700, 0), Color.White);
+                    }
+                    else
+                    {
+                        _spriteBatch.DrawString(font, $"{minutes + " : " + seconds}", new Vector2(700, 10), Color.White);
+                    }
+
+                    // display health if player(s) is alive
+                    if (EntityLists.playerList.Count != 0)
+                    {
+                        _spriteBatch.DrawString(font, $"Health: { EntityLists.playerList[0].GetHealth().ToString()}", new Vector2(20, 10), Color.White);
+                    }
+                    else
+                    {
+                        _spriteBatch.DrawString(font, "Game Over", new Vector2(300, this.Window.ClientBounds.Height / 2), Color.White);
+                        _spriteBatch.DrawString(font, "Press ESC to exit", new Vector2(300, this.Window.ClientBounds.Height / 2 + 100), Color.White);
+                        EntityLists.enemyList.Clear();
+                        EntityLists.playerBulletList.Clear();
+                        EntityLists.enemyBulletList.Clear();
+                    }
+
+                    if (win)
+                    {
+                        _spriteBatch.DrawString(font, "Winner", new Vector2(300, this.Window.ClientBounds.Height / 2), Color.White);
+                        _spriteBatch.DrawString(font, "Press ESC to return to menu", new Vector2(200, this.Window.ClientBounds.Height / 2 + 100), Color.White);
+                        EntityLists.enemyList.Clear();
+                        EntityLists.playerBulletList.Clear();
+                        EntityLists.enemyBulletList.Clear();
+                    }
+                    break;
             }
 
             _spriteBatch.End();
