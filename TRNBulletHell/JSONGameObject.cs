580385a6c0db71aefdae4b336f0ed97655262e3d
Minh <77561805+mn-0000@users.noreply.github.com>
2022-05-01 11:26:03 -0700

    Add json object class

---
+++ TRNBulletHell/JSONGameObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TRNBulletHell
+{
+    class JSONGameObject
+    {
+        public int ID { get; set; }
+        public int Time { get; set; }
+        public string EnemyType { get; set; }
+        public int EnemyAmount { get; set; }
+        public int Interval { get; set; }
+        public int BulletRate { get; set; }
+        public int Damage { get; set; }
+    }
+}
