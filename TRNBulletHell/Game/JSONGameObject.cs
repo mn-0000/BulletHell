1ce16b9422c78d99d22acd85fdb60056b1755601
Minh <77561805+mn-0000@users.noreply.github.com>
2022-05-01 13:22:16 -0700

    Added JSON game object class

---
+++ TRNBulletHell/Game/JSONGameObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TRNBulletHell.Game
+{
+    public class JSONGameObject
+    {
+        public int Time { get; set; } // maximum time alloted for the wave
+        public string EnemyType { get; set; } // enemy type of the wave
+        public int EnemyAmount { get; set; } // number of enemies in the wave
+        public int Interval { get; set; } // amount of time to wait between each enemy spawn
+        public int BulletRate { get; set; } // rate at which the enemy(ies) would spawn bullets
+        public int Damage { get; set; } // damage of each bullet
+    }
+}
