bd5c988130b109a8ce5ddde315b0a92f299bed70
Colin Willis <cwillis@eecs.wsu.edu>
2022-05-01 21:45:53 -0700

    Deleted files that were not needed. Moved JSON script file to a new folder inside the solution folder.

--- TRNBulletHell/GameDriver.cs
+++ TRNBulletHell/GameDriver.cs
@@ -92,7 +92,7 @@ namespace TRNBulletHell
            
             // Process user-provided JSON stage file
             // Edit path to file here
-            string stageDetails = File.ReadAllText("C:\\Users\\miran\\OneDrive\\Desktop\\main\\teamreptileninjas\\Sample.json");
+            string stageDetails = File.ReadAllText("C:\\Users\\Colin Willis\\Desktop\\teamreptileninjas\\TRNBulletHell\\JSON-Script\\Sample.json");
             RootObject jsonObject = JsonSerializer.Deserialize<RootObject>(stageDetails);
             
             // Process wave data and create waves.
